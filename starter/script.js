570b4e5d13eec25ab90b8538e6fdf320aeae7961
Edward Leung <edward_leung_12@hotmail.com>
2020-10-26 20:37:34 +0000

    added too high / too low scenario as well

--- starter/script.js
+++ starter/script.js
@@ -11,5 +11,9 @@ document.querySelector('.check').addEventListener('click', function() {
     document.querySelector('.message').textContent = 'No number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!';
   }
 });
