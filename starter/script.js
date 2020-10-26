980d5542a1affe63864dd8fcc9532c350015a488
Edward Leung <edward_leung_12@hotmail.com>
2020-10-26 20:35:36 +0000

    added correct number scenario

--- starter/script.js
+++ starter/script.js
@@ -1,10 +1,15 @@
 'use strict';
 
+const secretNumber = Math.floor(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function() {
   const guess = parseInt(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = ' No number!';
+    document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number!';
   }
 });
