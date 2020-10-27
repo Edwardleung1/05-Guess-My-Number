40412746316a395390a3b2ab28fef211a67936c5
Edward Leung <edward_leung_12@hotmail.com>
2020-10-27 12:30:16 +0000

    refactoring the code

--- starter/script.js
+++ starter/script.js
@@ -25,21 +25,10 @@ document.querySelector('.check').addEventListener('click', function() {
       document.querySelector('.highscore').textContent = highScore;
     }
 
-    // When guess is too high
-  } else if (guess > secretNumber) {
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high!';
-      score -= 1;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
-      document.querySelector('.score').textContent = 0;
-    }
-
-    // When guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low!';
+      document.querySelector('.message').textContent = guess > secretNumber ? 'Too high!' : 'Too low!';
       score -= 1;
       document.querySelector('.score').textContent = score;
     } else {
